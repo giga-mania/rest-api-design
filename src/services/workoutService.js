82ad27c25cdc526c667d98597e85b966ea456249
giga-mania <hil.giga@gmail.com>
2023-05-31 12:08:32 +0100

    Add workout services

---
+++ src/services/workoutService.js
@@ -0,0 +1,48 @@
+const Workout = require("../database/Workout")
+const { v4: uuid } = require("uuid");
+
+
+const getAllWorkouts = () => {
+    return Workout.findAll()
+};
+
+const getOneWorkout = (id) => {
+    const workout = Workout.findById(id)
+
+    return workout
+};
+
+const createNewWorkout = (body) => {
+    const workOutToCreate = {
+        ...body,
+        createdAt: new Date().toLocaleString("en-GB", {timeZone: "UTC"}),
+        updatedAt: new Date().toLocaleString("en-GB", {timeZone: "UTC"}),
+        id: uuid()
+    }
+
+    const newWorkout = Workout.create(workOutToCreate)
+    return newWorkout
+};
+
+const updateOneWorkout = (id, updatesToMake) => {
+    const updatedWorkout = Workout.updateById(id, {
+        ...updatesToMake,
+        updatedAt: new Date().toLocaleString("en-GB", {timeZone: "UTC"})
+    })
+
+    return updatedWorkout
+};
+
+const deleteOneWorkout = (id) => {
+    const deletedWorkout = Workout.deleteById(id)
+
+    return deletedWorkout
+};
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+};
